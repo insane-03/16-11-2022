1ef200cfa149a72fb7454d10ebd255480c7e598b
insane-03 <118243063+insane-03@users.noreply.github.com>
2022-11-16 16:15:09 +0530

    Add files via upload
    passwordEncode

---
+++ Hash.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerEnc
+{
+    public class Hash
+    {
+        public void enCode(string password)
+        {
+          
+          byte[] passascii = Encoding.ASCII.GetBytes(password);
+          string[] hash = new string[passascii.Length];
+
+            try
+            {
+                for (int i = 0; i < (passascii.Length); i++)
+                {
+                    string result1 = Char.ToString(Convert.ToChar(passascii[i] + 3));
+                    //Console.WriteLine(result1);
+                    hash[i] = result1;
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString()+ " check the for loop");
+            }
+            
+            //Console.WriteLine(hash.Length);
+            Console.Clear();
+            Console.Write("\nYOUR HASH CODE FOR YOUR PASSWORD: "+ string.Join("", hash));
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace PasswordManagerEnc
+{
+    internal class Program
+    {
+        
+
+
+        static void view(string objName)
+        {
+            Console.Clear();
+            Console.WriteLine("\n<<<<<<<<<<<<<p -----PASSWORD MANAGER [ENCODING] $ Welcome," + objName + "------ >>>>>>>>>>>>>>");
+            Console.Write("\n\nEnter the password to encode:");
+            string password = Console.ReadLine();
+            Hash objen = new Hash();
+            objen.enCode(password);
+        }
+
+        public void Nuser(string objName)
+        {
+            
+        }
+        static int Ccheck()
+        {   
+            Console.Clear();
+            Console.Write("\nTo continue press 1 or press 2 :");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("\n<<<<<<<<<<<<< -----PASSWORD MANAGER [ENCODING]------ >>>>>>>>>>>>>>\n\n\n");
+            do
+            {
+                Console.Write("\nEnter the UserName:");
+                string objName = Console.ReadLine();
+                string[] userarray = {"gowtham"};
+                //Console.WriteLine("Hello" + objName);
+
+                foreach (string str in userarray)
+                {
+                    if (str == objName)
+                    {
+                        view(objName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nNeed to create new account ! Try again later ");
+                        Console.WriteLine("\nCome agin:");
+                        Console.ReadLine();
+                    }
+                }
+
+                
+                
+                
+                
+            }while (Ccheck() == 1);
+            Console.WriteLine("ok bye bye");
+            Console.ReadKey();
+        }
+    }
+}
