e9227fd01eb73faec5317b8e4abcb4b8bfbd0663
insane-03 <118243063+insane-03@users.noreply.github.com>
2022-11-16 14:36:27 +0530

    Add files via upload
    DataAbstraction

---
+++ DataAbstraction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_OOP
+{
+    public abstract class DataAbstraction
+    {
+        public abstract void Entry();
+        public abstract void Exit();
+    }
+
+    class Office : DataAbstraction
+    {   
+        public Office()
+        {
+            Console.WriteLine("\nHEY THIS IS DATA_ABSTRACTION");
+        }
+        public override void Entry()
+        {
+            Console.WriteLine("\nNOTED ! YOUR ENTERY");
+        }
+        public override void Exit()
+        {
+            Console.WriteLine("\nNOTED ! YOUR EXIT");
+        }
+
+        static void Main(string[] args)
+        {
+            Office OfficeObj = new Office();
+            OfficeObj.Entry();
+            Console.WriteLine(".\n.\n.\n.\n.\n.\n");
+            OfficeObj.Exit();
+            
+            Console.ReadKey();
+        }
+
+
+    }
+}
